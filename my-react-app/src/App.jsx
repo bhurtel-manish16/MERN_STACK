07425d32339276131518c76697761f97130e967b
Manish Bhurtel <bhurtel1manish@gmail.com>
2023-10-09 20:42:49 +0545

    Intro to React

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Welcome from "./components/Welcome";
+
+function App() {
+  return (
+    <div>
+      <button>Click</button>
+      <h1>Hello world</h1>
+      <Welcome />
+    </div>
+  );
+}
+export default App;
---
+++ my-react-app/src/components/Welcome.jsx
@@ -0,0 +1,10 @@
+// UserData
+function Welcome() {
+
+    const Display = () => {
+        console.log("Hello from function")
+    }
+    Display()
+    return <h2>Welcome to my page</h2>
+}
+export default Welcome
