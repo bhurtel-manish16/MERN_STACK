bae2c6d834699e79a557a0f230c0e22a12352dd2
Manish Bhurtel <bhurtel1manish@gmail.com>
2023-10-13 19:59:22 +0545

    Children in React

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,22 @@
 import Welcome from "./components/Welcome";
 import Shop from "./components/Shop";
+import Child from "./components/Child";
 
 function App() {
+  const product_list = ["Apple", "Dell", "HP", "Asus", "LG", "XYZ", "ABC"];
+  const shop_name = "ABCD PASAL";
 
+  const click = () => {
+    return console.log("Hi")
+  }
   return (
     <div>
       {/* <Welcome /> */}
-      <Shop></Shop>
+      {/* <Shop product_list={product_list} shop_name={shop_name}> </Shop> */}
+      <Child>
+        Hello from <b>App</b>
+        <button onClick={click}>Click me</button><br></br>
+      </Child>
     </div>
   );
 }
--- my-react-app/src/components/Shop.jsx
+++ my-react-app/src/components/Shop.jsx
@@ -2,8 +2,8 @@
 
 import { useState } from "react";
 
-function Shop() {
-  const product_list = ["Apple", "Dell", "HP", "Asus", "LG", "XYZ"];
+function Shop({product_list, shop_name}) {
+  // const product_list = ["Apple", "Dell", "HP", "Asus", "LG", "XYZ"];
 //   let click_no = 0;
   //   Hook
   let [click_no, set_click_no] = useState(-1);
@@ -20,7 +20,7 @@ function Shop() {
   //   }
   return (
     <>
-      <h1>BHURTEL COMPUTER PASAL</h1>
+      <h1>{shop_name}</h1>
       {/* {product_list.length == 0 ? <p>No item found!</p> : null} */}
       {product_list.length == 0 && <p>No item found!</p>}
       <ul className="list-group">
